45dedb49beb314ea363f0af8f62b7c726f2bcae2
Jhonatas Willian <jhonatas.willian.dev@gmail.com>
2026-02-06 20:18:24 -0300

    first commit

---
+++ src/main/java/com/jhonataswillian/cinelog/CinelogApplication.java
@@ -0,0 +1,13 @@
+package com.jhonataswillian.cinelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinelogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jhonataswillian/cinelog/CinelogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonataswillian.cinelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinelogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
